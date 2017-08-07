7f0ca79b49b407556901b0c3c783739d2ba5154e
Mike Tsou <mr.tsou@gmail.com>
2017-08-07 00:04:48 -0700

    Create PostIndex component and fetch_post action creator

--- src/index.js
+++ src/index.js
@@ -2,16 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import { BrowserRouter, Route } from 'react-router-dom';
+import PostIndex from './components/PostIndex';
 
-
-// import App from './components/App';
-// import reducers from './reducers';
+import reducers from './reducers';
 
 // middlewares block, modify, or let actions pass through as theyre created, before they hit a reducer. sort of like a gate keeper
-// const createStoreWithMiddleware = applyMiddleware()(createStore);
+const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
-  // <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  // </Provider>
+  // Provider makes the Redux store available to the connect() calls in the component hierarchy below. Normally, you can’t use connect() without wrapping a parent or ancestor component in <Provider>.
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <BrowserRouter>
+      <div>
+        <Route path="/" component={PostIndex} />
+      </div>
+    </BrowserRouter>
+  </Provider>
   , document.querySelector('.container'));
